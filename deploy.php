ed9739eb6e7c7e2360ccfc6cf795dc46df46df3c
gsuess <gsuess@gs-laptop.lan>
2013-04-19 14:06:35 +0200

    Fixed strict error and added further verbosity.

--- deploy.php
+++ deploy.php
@@ -16,16 +16,27 @@ $payload = Payload::receive();
 if ($payload) {
 	echo 'Payload received at ', date(DateTime::ISO8601), PHP_EOL;
 	
-	array_walk(parse_ini_file('config.ini', true), function (array $target, $name) use($payload) {
+	$config = parse_ini_file('config.ini', true);
+	
+	array_walk($config, function (array $target, $name) use($payload) {
 		if (isset($target['payload']))
 			throw new InvalidArgumentException('Bad configuration argument "payload" encounterred');
 		
 		extract($target);
 		
 		$git = new Git($path);
+
+		$target_name = $git->name();
+		$target_ref = $git->ref();
 		
-		if ($payload->reqository->name !== $git->name() or $payload->ref !== $git->ref()) {
+		if ($payload->reqository->name !== $target_name or $payload->ref !== $target_ref) {
 			echo "Payload doesn't apply to $name", PHP_EOL;
+			
+			if ($payload->ref !== $target_ref)
+				echo "$name has ref $target_ref, but incoming is $payload->ref", PHP_EOL;
+			if ($payload->reqository->name !== $target_name)
+				echo "$name has name $target_name, but incoming is {$payload->reqository->name}", PHP_EOL;
+
 			return;
 		}
 			
